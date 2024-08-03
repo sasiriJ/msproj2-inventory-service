e1b53cf40bcc8b253179510de8e558db9394d894
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2024-08-03 15:23:40 +0530

    Writing test case for inventory service

--- src/test/java/com/sasiri/productapp/inventoryservice/InventryServiceApplicationTests.java
+++ src/test/java/com/sasiri/productapp/inventoryservice/InventryServiceApplicationTests.java
@@ -1,15 +1,55 @@
 package com.sasiri.productapp.inventoryservice;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class InventryServiceApplicationTests {
 
+    @ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest");
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mySQLContainer.start();
+    }
     @Test
-    void contextLoads() {
+    void shouldReadInventory() {
+        var response = RestAssured.given()
+                .when()
+                .get("/api/inventory?skuCode=iphone_15&quantity=1")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().response().as(Boolean.class);
+        assertTrue(response);
+
+        var negativeResponse = RestAssured.given()
+                .when()
+                .get("/api/inventory?skuCode=iphone_15&quantity=1000")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .extract().response().as(Boolean.class);
+        assertFalse(negativeResponse);
+
     }
 
 }
